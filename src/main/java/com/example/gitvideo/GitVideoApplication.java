16b4e684cf1b93ba0771df8ea60e3bf2abcff7e8
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-02-01 10:45:31 +0400

    init

---
+++ src/main/java/com/example/gitvideo/GitVideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitVideoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitVideoApplication.class, args);
+  }
+
+}
