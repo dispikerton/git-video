847f290146b2bd8f90ea81fc8d2c88f0fd3cacd5
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-03-21 11:11:25 +0400

    Add start engine

--- src/main/java/com/example/gitvideo/Car.java
+++ src/main/java/com/example/gitvideo/Car.java
@@ -1,9 +1,16 @@
 package com.example.gitvideo;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class Car {
   private int id;
   private String brand;
+
+
+  public void startEngine() {
+    log.info("др-др-др-др-др");
+  }
 }
